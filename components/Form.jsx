a102efa1fbec19325558629844c420df6d08e77a
simhaeun <7351_@daum.net>
2023-05-08 16:19:10 +0900

    form

--- components/Form.jsx
+++ components/Form.jsx
@@ -1,23 +1,15 @@
 import React from "react";
 import { Widget } from "@typeform/embed-react";
-import { Box, Container } from "@mui/material/";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 const Form = () => {
-  const theme = createTheme();
-
   return (
-    <ThemeProvider theme={theme}>
-      <Container>
-        <Box>
-          <Widget
-            id="z8waQfLn"
-            style={{ width: "600px", height: "700px" }}
-            className="my-form"
-          />
-        </Box>
-      </Container>
-    </ThemeProvider>
+    <>
+      <Widget
+        id="z8waQfLn"
+        style={{ width: "600px", height: "700px" }}
+        className="my-form"
+      />
+    </>
   );
 };
 export default Form;
