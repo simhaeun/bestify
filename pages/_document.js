1dbb30eb47efd69ea49d2f3bae095258053f0639
simhaeun <7351_@daum.net>
2023-05-16 15:57:50 +0900

    ✨ 채널톡

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,4 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
@@ -7,7 +7,50 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function() {
+              var w = window;
+              if (w.ChannelIO) {
+                return (window.console.error || window.console.log || function(){})('ChannelIO script included twice.');
+              }
+              var ch = function() {
+                ch.c(arguments);
+              };
+              ch.q = [];
+              ch.c = function(args) {
+                ch.q.push(args);
+              };
+              w.ChannelIO = ch;
+              function l() {
+                if (w.ChannelIOInitialized) {
+                  return;
+                }
+                w.ChannelIOInitialized = true;
+                var s = document.createElement('script');
+                s.type = 'text/javascript';
+                s.async = true;
+                s.src = 'https://cdn.channel.io/plugin/ch-plugin-web.js';
+                s.charset = 'UTF-8';
+                var x = document.getElementsByTagName('script')[0];
+                x.parentNode.insertBefore(s, x);
+              }
+              if (document.readyState === 'complete') {
+                l();
+              } else if (window.attachEvent) {
+                window.attachEvent('onload', l);
+              } else {
+                window.addEventListener('DOMContentLoaded', l, false);
+                window.addEventListener('load', l, false);
+              }
+            })();
+            ChannelIO('boot', {
+              "pluginKey": "${process.env.API_CHANNEL_KEY}"
+            });
+            `,
+          }}
+        />
       </body>
     </Html>
-  )
+  );
 }
